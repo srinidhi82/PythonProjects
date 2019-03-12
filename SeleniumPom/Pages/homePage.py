997ced02e8038550aa48d11a518bd5ef54276904
Srinidhi <srinidhi23@gmail.com>
2019-03-12 22:18:48 +0000

    HomePage class

---
+++ SeleniumPom/Pages/homePage.py
@@ -0,0 +1,12 @@
+class HomePage():
+    def __init__(self,driver):
+        self.driver = driver
+
+        self.welcome_link_id = "welcome"
+        self.logout_link_text = "Logout"
+
+    def click_welcome_link(self):
+        self.driver.find_element_by_id(self.welcome_link_id).click()
+
+    def click_logout(self):
+        self.driver.find_element_by_link_text(self.logout_link_text).click()
