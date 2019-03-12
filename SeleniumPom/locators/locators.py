27b1c7e6d13ab9f79b834c70a7eb1929cb74bbda
Srinidhi <srinidhi23@gmail.com>
2019-03-12 22:42:40 +0000

    Adding Locators class

---
+++ SeleniumPom/locators/locators.py
@@ -0,0 +1,14 @@
+class Locators():
+
+#Login Page Objects
+
+    username_textbox_id = "txtUsername"
+    password_textbox_id = "txtPassword"
+    login_button_id = "btnLogin"
+
+#Home Page objects
+
+    welcome_link_id = "welcome"
+    logout_link_text = "Logout"
+
+
