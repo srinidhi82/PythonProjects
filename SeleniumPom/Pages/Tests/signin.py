032ccd12b03461ef7f480902e293b67194ba2979
Srinidhi <srinidhi23@gmail.com>
2019-03-12 22:19:21 +0000

    CHanges to test file based on page objects created

--- SeleniumPom/Pages/Tests/signin.py
+++ SeleniumPom/Pages/Tests/signin.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
+from SeleniumPom.Pages.loginPage import LoginPage
+from SeleniumPom.Pages.homePage import HomePage
 
 class LoginTest(unittest.TestCase):
 
@@ -13,12 +15,24 @@ class LoginTest(unittest.TestCase):
 
 
     def test_Login(self):
-        self.driver.get('https://opensource-demo.orangehrmlive.com/')
-        self.driver.find_element_by_id("txtUsername").send_keys("Admin")
-        self.driver.find_element_by_id("txtPassword").send_keys("admin123")
-        self.driver.find_element_by_id("btnLogin").click()
-        self.driver.find_element_by_id("welcome").click()
-        self.driver.find_element_by_link_text("Logout").click()
+        driver = self.driver
+
+        driver.get('https://opensource-demo.orangehrmlive.com/')
+
+        login = LoginPage(driver)
+        login.enter_username("Admin")
+        login.enter_password("admin123")
+        login.click_login()
+
+        homepage = HomePage(driver)
+        homepage.click_welcome_link()
+        homepage.click_logout()
+
+        # self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+        # self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+        # self.driver.find_element_by_id("btnLogin").click()
+        # self.driver.find_element_by_id("welcome").click()
+        # self.driver.find_element_by_link_text("Logout").click()
         time.sleep(2)
 
     @classmethod
