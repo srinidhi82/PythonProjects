a344910e468c9a88c554e5c9ba8de288ffa19962
Srinidhi <srinidhi23@gmail.com>
2019-03-12 22:24:08 +0000

    fixed the arguments

--- SeleniumPom/Pages/loginPage.py
+++ SeleniumPom/Pages/loginPage.py
@@ -9,11 +9,11 @@ class LoginPage():
 
     def enter_username(self,username):
         self.driver.find_element_by_id(self.username_textbox_id).clear()
-        self.driver.find_element_by_id(self.username_textbox_id).send_keys("username")
+        self.driver.find_element_by_id(self.username_textbox_id).send_keys(username)
 
     def enter_password(self,password):
         self.driver.find_element_by_id(self.password_textbox_id).clear()
-        self.driver.find_element_by_id(self.password_textbox_id).send_keys("password")
+        self.driver.find_element_by_id(self.password_textbox_id).send_keys(password)
 
     def click_login(self):
         self.driver.find_element_by_id(self.login_button_id).click()
