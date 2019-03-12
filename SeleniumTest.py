212253c773322b9f5faf69bfed723c2e6c343c3f
Srinidhi <srinidhi23@gmail.com>
2019-03-12 17:31:27 +0000

    Add KEYS

--- SeleniumTest.py
+++ SeleniumTest.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome('/Users/srinidhik/SeleniumWebdriver/chromedriver')
 browser.get('https://www.seleniumhq.org')
@@ -7,10 +7,12 @@ browser.get('https://www.seleniumhq.org')
 browser.find_element_by_link_text('Download')
 
 element1 = browser.find_element_by_link_text('Download')
+element1.text
 element1.click()
 
 elem = browser.find_element_by_link_text('Projects')
 elem.click()
-searchBar = browser.find_elements_by_id('q')
-searchBar.send_keys('download')
+searchBar = browser.find_element_by_id('q')
+searchBar.send_keys('java')
 
+searchBar.send_keys(Keys.ENTER)
